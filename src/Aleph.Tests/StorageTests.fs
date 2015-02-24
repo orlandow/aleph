b897b1a0a765e828ee5145021bb78d34dd06fb5a
Orlando William <0rlandw@gmail.com>
2015-02-23 22:47:41 -0500

    testing a record storage

---
+++ src/Aleph.Tests/StorageTests.fs
@@ -0,0 +1,25 @@
+﻿module SerializationTests
+
+open NUnit.Framework
+open Raven.Client.Embedded
+open Geography
+open System
+
+let store = new EmbeddableDocumentStore(RunInMemory = true)
+store.Initialize() |> ignore
+
+let stores (obj:'a) =
+    use session = store.OpenSession()
+    session.Store(obj, "id")
+    session.SaveChanges()
+
+    use session = store.OpenSession()
+    let loaded = session.Load<'a>("id")
+
+    Assert.AreEqual(obj, loaded)
+
+
+[<Test>]
+let ``can store a country`` () =
+    let c = { id = Guid.NewGuid(); name = "Cuba"; continent = America }
+    stores c
