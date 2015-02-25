5911ccd87269c93247a4cbddfce223e3a7916236
Orlando William <0rlandw@gmail.com>
2015-02-24 23:32:41 -0500

    added time types (first draft)

---
+++ src/Aleph.Domain/Time.fs
@@ -0,0 +1,35 @@
+﻿[<AutoOpen>]
+module Time
+
+type Day = int
+
+type Month = 
+    | January
+    | February
+    | March
+    | April
+    | May
+    | June
+    | July
+    | August
+    | September
+    | October
+    | November
+    | December
+
+type Year = 
+    | BC of int
+    | Year of int
+
+type Century = string
+
+type Date = 
+    | CenturyDate of Century
+    | YearDate of Year
+    | MonthDate of Month * Year
+    | DayDate of Day * Month * Year
+ 
+type Period =
+    | Unknown
+    | Single of Date
+    | Exact of start:Date * finish:Date
