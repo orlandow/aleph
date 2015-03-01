bef64717eb2b3ec041b1bbd04c3c70a427b5d9c6
Orlando William <0rlandw@gmail.com>
2015-03-01 13:18:02 -0500

    added people

---
+++ src/Aleph.Domain/People.fs
@@ -0,0 +1,12 @@
+﻿module People
+
+type Lifespan = 
+    | Alive of birth:Date
+    | Dead of birth:Date * death:Date
+
+type Person = {
+    name: string
+    fullName: string
+    lifespan: Lifespan
+    country: Country
+}
