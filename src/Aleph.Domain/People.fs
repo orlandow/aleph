aace6d5700f0705f37c542dac7fb84dd5b487f88
Orlando William <0rlandw@gmail.com>
2015-03-03 14:03:51 -0500

    added profession

--- src/Aleph.Domain/People.fs
+++ src/Aleph.Domain/People.fs
@@ -4,9 +4,12 @@ type Lifespan =
     | Alive of birth:Date
     | Dead of birth:Date * death:Date
 
+type Profession = string
+
 type Person = {
     name: string
     fullName: string
     lifespan: Lifespan
     country: Country
+    profession: Profession
 }
