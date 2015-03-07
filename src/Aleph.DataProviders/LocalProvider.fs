f6cdc237d6868836ed436d83569245d2a660e9af
Orlando William <0rlandw@gmail.com>
2015-03-07 16:38:21 -0500

    added local provider and suggestions draft

---
+++ src/Aleph.DataProviders/LocalProvider.fs
@@ -0,0 +1,55 @@
+﻿namespace DataProviders
+
+module private IO =
+    open System.IO
+
+    let folder = @"d:\development\aleph data"
+
+    let files = 
+        Directory.EnumerateFiles(folder, "*.json")
+        |> Seq.map (fun x -> 
+            (Path.GetFileNameWithoutExtension x,
+             File.ReadAllText x))
+
+module private Json =
+    open Newtonsoft.Json
+    open Newtonsoft.Json.Linq
+
+    let jsonToMap = fun x -> JsonConvert.DeserializeObject<Map<string,obj>>(x)
+    let fromJ str (ob:obj) = 
+        match (ob :?> JObject).TryGetValue(str) with
+        | (true, value) -> Some <| value.ToString()
+        | _ -> None
+
+
+module LocalProvider =
+
+    let data = 
+        IO.files
+        |> Seq.map (fun (name,value) -> (name, Json.jsonToMap value))
+
+    let byName str =
+        data 
+        |> Seq.find (fst >> ((=) str))
+        |> snd
+
+    let suggest str =
+        let data = byName str
+
+        let input = data |> Map.tryFind "Input interpretation" 
+        let basic = data |> Map.tryFind "Basic information" 
+
+        let get str = Option.bind (Json.fromJ str)
+
+        let name = input |> get "name"
+        let profession = input |> get "profession"
+        let fullname = basic |> get "full name"
+
+        {
+            name = name
+            fullname = fullname
+            profession = profession
+            lifespan = None
+            images = None
+            raw = None
+        }
---
+++ src/Aleph.DataProviders/Types.fs
@@ -0,0 +1,17 @@
+﻿namespace DataProviders
+
+open People
+
+type Image = byte[]
+
+type Suggestions = {
+    name: string option
+    fullname: string option
+    lifespan: Lifespan option
+    profession: Profession option
+    images: Image option
+
+    raw: string option
+}
+
+type Provider = string -> Suggestions
