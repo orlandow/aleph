6239ee9f57d20a7fd74e90012509264e7a10ad97
Orlando William <0rlandw@gmail.com>
2015-03-08 17:00:00 -0400

    added reflection functions to Misc project

---
+++ src/Misc/Reflection.fs
@@ -0,0 +1,20 @@
+﻿[<RequireQualifiedAccess>]
+module Reflection
+
+open Microsoft.FSharp.Reflection
+
+let private like a b =
+    let trim (x:string) = x.Trim().ToLowerInvariant()
+    trim a = trim b
+
+let toDU<'a> str =
+    let t = typedefof<'a>
+    let u = FSharpType.GetUnionCases(t)
+            |> Array.find (fun u -> like u.Name str)
+        
+    FSharpValue.MakeUnion(u, [||]) :?> 'a
+
+let toStr du =
+    let t = du.GetType()
+    let u,_ = FSharpValue.GetUnionFields(du, t)
+    u.Name
