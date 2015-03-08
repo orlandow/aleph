27a5a035ee28819f333da518882703bd80b3ca2f
Orlando William <0rlandw@gmail.com>
2015-03-08 17:03:10 -0400

    test for reflection misc functions

---
+++ src/Aleph.Tests/MiscTests.fs
@@ -0,0 +1,10 @@
+﻿module MiscTests
+
+open NUnit.Framework
+
+type Enum = One | Two | Three
+
+[<Test>]
+let ``can convert str to du`` () =
+    let one = "One" |> Reflection.toDU<Enum>
+    Assert.AreEqual(One, one)
