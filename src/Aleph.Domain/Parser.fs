3d03008d756210706f96c2c26dd9b63613e01aaf
Orlando William <0rlandw@gmail.com>
2015-03-08 16:22:41 -0400

    parsing day

--- src/Aleph.Domain/Parser.fs
+++ src/Aleph.Domain/Parser.fs
@@ -1,24 +1,38 @@
 ﻿module Parser
 
 open System
-open System.Text.RegularExpressions
 
-let [<Literal>] yearBC = @"(?<year>\d+)\s*(bc|b.c|b.c.|BC)"
+module private Regex = 
+    open System.Text.RegularExpressions
 
-let Re re f =
-    fun str ->
-        let re = new Regex(re)
-        let m = re.Match(str)
-        if m.Success
-            then Some (f <| m.Groups)
-            else None
+    let [<Literal>] yearBC = @"(?<year>\d+)\s*(bc|b.c|b.c.|BC)"
+    let [<Literal>] year = @"^\d+$"
 
-let (|YearBC|_|) = Re yearBC (fun m -> Int32.Parse(m.["year"].Value))
-let (|Year|_|) = Re "^\d+$" (fun m -> Int32.Parse(m.[0].Value))
+    let matches re f =
+        fun str ->
+            let re = new Regex(re)
+            let m = re.Match(str)
+            if m.Success
+                then Some (f <| m.Groups)
+                else None
+
+module private DateTime =
+    let private dfi = new Globalization.DateTimeFormatInfo()
+    let parse str =
+        match DateTime.TryParse(str) with
+        | (true, d) -> Some (d.Day, d.Month, d.Year)
+        | _ -> None
+
+    let toMonth i = dfi.GetMonthName i |> Reflection.toDU<Month>
+
+let (|YearBC|_|) = Regex.matches Regex.yearBC (fun m -> Int32.Parse(m.["year"].Value))
+let (|Year|_|) = Regex.matches Regex.year (fun m -> Int32.Parse(m.[0].Value))
+let (|DateTime|_|) str = DateTime.parse str
 
 type Date with 
     static member Parse str = 
         match str with
         | Year year -> Year year
         | YearBC year -> Year (-year)
+        | DateTime (d,m,y) -> Day (d, m |> DateTime.toMonth, y)
         | _ -> failwith "bad input"
--- src/Aleph.Tests/DateParsingTests.fs
+++ src/Aleph.Tests/DateParsingTests.fs
@@ -16,3 +16,8 @@ let ``can parse a simple year`` () =
 let ``can parse a BC year`` () =
     "300 BC" |> parses (Year -300)
     "5000 BC" |> parses (Year -5000)
+
+[<Test>]
+let ``can parse a date in long format`` () =
+    "Thursday, August 24, 1899" |> parses (Day (24, August, 1899))
+    "Saturday, June 14, 1986" |> parses (Day (14, June, 1986))
