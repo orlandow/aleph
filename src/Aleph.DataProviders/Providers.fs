613bb006e07404e0bef0407a35627b5fa5e123b5
Orlando William <0rlandw@gmail.com>
2015-03-08 20:53:19 -0400

    added suggest to providers

--- src/Aleph.DataProviders/Providers.fs
+++ src/Aleph.DataProviders/Providers.fs
@@ -4,3 +4,14 @@ module Providers =
     let all : Provider list =
         [ LocalProvider.suggest 
           BingImageProvider.suggest ]
+
+    let suggest str =
+        all
+        |> Seq.map (fun p -> 
+            async {
+                try
+                    let! result = p str
+                    return Success result 
+                with
+                | ex -> return Failure ex })
+        |> Async.Parallel
