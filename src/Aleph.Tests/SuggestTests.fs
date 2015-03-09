9ac272f87ee7eb1494e25e48e9e728a38ab181fa
Orlando William <0rlandw@gmail.com>
2015-03-08 23:33:41 -0400

    data provider tests

---
+++ src/Aleph.Tests/SuggestTests.fs
@@ -0,0 +1,15 @@
+﻿module SuggestTests
+
+open NUnit.Framework
+open DataProviders
+
+[<Test;Ignore("not unit test")>]
+let ``can suggest`` () =
+    let suggestions = 
+        Providers.suggest "borges"
+        |> Async.RunSynchronously
+        |> Array.choose (function
+            | Success s -> Some s
+            | Failure e -> None )
+
+    Assert.IsNotEmpty(suggestions)
