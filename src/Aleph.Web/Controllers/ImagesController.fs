9d5e10dc8dda22a1a92e5850777b2a7df3b299e2
Orlando William <0rlandw@gmail.com>
2015-03-18 23:53:51 -0400

    adding ImagesController

---
+++ src/Aleph.Web/Controllers/ImagesController.fs
@@ -0,0 +1,18 @@
+﻿namespace Aleph.Web.Controllers
+
+open System
+open System.Collections.Generic
+open System.Linq
+open System.Web
+open System.Web.Mvc
+open System.Web.Helpers
+
+type ImagesController() =
+    inherit Controller()
+
+    member this.Get(id:string, width, height) = 
+        let img = new WebImage(this.Server.MapPath("~/assets/imgs/question.jpg"))
+        img.Resize(width, height, true)
+           .Crop(1,1)
+           .Write() 
+           |> ignore
