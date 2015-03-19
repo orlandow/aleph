92893bc8ece27a84bd9dd13477afa4b9960887fd
Orlando William <0rlandw@gmail.com>
2015-03-19 00:45:11 -0400

    images module

--- src/Aleph.Web/Controllers/ImagesController.fs
+++ src/Aleph.Web/Controllers/ImagesController.fs
@@ -6,13 +6,23 @@ open System.Linq
 open System.Web
 open System.Web.Mvc
 open System.Web.Helpers
+open Aleph.Web.Model
 
 type ImagesController() =
     inherit Controller()
 
-    member this.Get(id:string, width, height) = 
-        let img = new WebImage(this.Server.MapPath("~/assets/imgs/question.jpg"))
-        img.Resize(width, height, true)
-           .Crop(1,1)
-           .Write() 
-           |> ignore
+    member this.question() = this.Server.MapPath("~/assets/imgs/question.jpg")
+
+    member this.Get(id, width, height) = 
+        async {
+            let! result = Images.get id
+            let img = match result with
+                      | Some img -> new WebImage(img)
+                      | None -> new WebImage(this.question())
+
+            img
+                .Resize(width, height)
+                .Crop(1,1)
+                .Write()
+                |> ignore
+        } |> Async.RunSynchronously
---
+++ src/Aleph.Web/Model/Images.fs
@@ -0,0 +1,24 @@
+﻿namespace Aleph.Web.Model
+
+module Images =
+    open Raven.Client.Document
+    open Raven.Client.FileSystem
+    open System.IO
+
+    let store = new FilesStore()
+    store.Initialize() |> ignore
+    
+    let get (id:string) = 
+        use session = store.OpenAsyncSession()
+        async {
+            let! stream = Async.AwaitTask <| session.DownloadAsync(id)
+            return match stream with
+                   | null -> None
+                   | stream -> Some stream
+        }
+
+    let save (id:string) (data:byte[]) = 
+        use session = store.OpenAsyncSession()
+        let stream = new MemoryStream(data)
+        session.RegisterUpload(id, stream)
+        session.SaveChangesAsync().RunSynchronously()
